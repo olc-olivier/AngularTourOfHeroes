2b9015fbfeb13cbda3dfb79f9fc8336a89d99cd2
Olivier Chantereau <o.chantereau@itecor.com>
2016-03-03 16:15:45 +0100

    no message

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by o.chantereau on 01.03.2016.
+ */
+import {bootstrap}        from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {TohComponent} from "./toh.component";
+bootstrap(TohComponent, [
+    ROUTER_PROVIDERS
+]);
---
+++ app/toh.component.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by o.chantereau on 03.03.2016.
+ */
+import {Component}         from 'angular2/core';
+import {HTTP_PROVIDERS}    from 'angular2/http';
+import {HeroListComponent} from './hero-list.component';
+import {HeroService}       from './hero.service';
+@Component({
+    selector: 'my-toh',
+    template: `
+  <h1>Tour of Heroes</h1>
+  <hero-list></hero-list>
+  `,
+    directives:[HeroListComponent],
+    providers: [
+        HTTP_PROVIDERS,
+        HeroService,
+    ]
+})
+export class TohComponent { }
